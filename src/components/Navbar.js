10b2499443ca2af09db7761e158d7fc2d4b4778c
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-12-17 22:56:30 +0530

    made the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { BsCalendarDate } from "react-icons/bs";
+import { AiOutlinePlus } from "react-icons/ai";
 
 function Navbar() {
   return (
-    <div className='w-screen py-8 px-4 flex flex-row'>
+    <div className="w-screen py-8 px-4 flex flex-col space-y-3 items-start">
+      <font className="border rounded px-2 py-1">
         Employee Movement Breakdown
+      </font>
+
+      <div className="flex flex-row space-x-8">
+        <button className="border rounded px-2 py-1 flex flex-row space-x-2 items-center active:bg-gray-50">
+          <BsCalendarDate />
+          <font>2019</font>
+        </button>
+        <button className="border rounded px-2 py-1 flex flex-row space-x-2 items-center active:bg-gray-50">
+          <AiOutlinePlus />
+          <font>Add Filter</font>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
