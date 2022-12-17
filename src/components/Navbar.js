e41ce879bc5cabca91a07215b1c660e9e1a0a6f1
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-12-17 22:50:29 +0530

    Starting the Navbar || added the charts

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='w-screen py-8 px-4 flex flex-row'>
+        Employee Movement Breakdown
+    </div>
+  )
+}
+
+export default Navbar
